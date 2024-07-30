1927fcdd2bee668561d2bc721e20a6af76b170cf
oluwakorede <oluwakorede.idowu@stu.cu.edu.ng>
2024-07-30 13:24:39 +0100

    puff added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      
-    </section>
+    <main className="flex border justify-center items-center h-[600px] bg-orange-400 border-green-500">
+
+
+
+    <h1 className="text-center rounded-[15px] px-[20px] h-fit bg-green-400
+     border text-[30px]">Puff-Puff and Chicken</h1>
+
+
+    </main>
+
+
+
   );
 }
