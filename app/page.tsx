e2728ba3bac219450d06ee32ba89eecb65baac61
oluwakorede <oluwakorede.idowu@stu.cu.edu.ng>
2024-07-30 13:34:39 +0100

    Power Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex border justify-center items-center h-[600px] bg-orange-400 border-green-500">
-
-
+    <main className="flex border justify-center items-center h-[100vh] bg-orange-400 border-green-500">
 
     <h1 className="text-center rounded-[15px] px-[20px] h-fit bg-green-400
-     border text-[30px]">Puff-Puff and Chicken</h1>
+     border text-[30px]">Power Over Ethernet</h1>
 
 
     </main>
-
-
 
   );
 }
